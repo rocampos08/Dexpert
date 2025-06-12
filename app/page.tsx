2c189bbefcfb1d9f9b8c0f9cd6dd3fdd0e9e000d
LAPT-CSH-50 <cshtd.computacion@gmail.com>
2025-06-12 09:36:21 -0600

    About us

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { ExploreProjects } from "./(main)/(routes)/(root)/components/ExploreProjects";
-import { ExperienceBanner, Header } from "./(main)/(routes)/(root)/components";
+import { Aboutus, ExperienceBanner, Header } from "./(main)/(routes)/(root)/components";
 import CoverParticles from "./(main)/(routes)/(root)/components/ExperienceBanner/CoverParticles";
 import { getHomeProjects } from "@/actions/getHomeProjects";
 import ProjectsView from "./(main)/(routes)/(root)/components/ProjectsView/ProjectsView";
@@ -16,6 +16,7 @@ export default async function Home() {
       
       <Header/>
       <ExperienceBanner/>
+      <Aboutus/>
       <ProjectsView title="Our available projects" projects={listProjects}/>
     </div>
   );
