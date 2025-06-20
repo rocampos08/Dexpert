58d73a8de6bcf14802cbee9e1e07f61472bd51b1
Rodrigo Campos <roantocampos@gmail.com>
2025-06-20 00:10:49 -0600

    Refactor RootLayout component by removing unused font imports and simplifying the structure for improved readability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
-
 import type { Metadata } from "next";
-import {  Space_Grotesk } from "next/font/google";
-import "./globals.css"
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
-
-
-const spaceGrotesk = Space_Grotesk({
-  variable: "--font-space-grotesk",
-  subsets: ["latin"]
-})
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Dexpert",
@@ -19,24 +9,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${spaceGrotesk.variable} ${spaceGrotesk.className}  `}
-      
-      >
-        <div className=" bg-gradient-to-br from-[#E3F2FD] via-white to-[#BBDEFB]">
-            <main >
-        {children} 
-        </main>
-        </div>
-        
-      </body>
-    </html>
+      <html lang="en">
+        <body className="font-sans ">
+          <div className="bg-gradient-to-br from-[#E3F2FD] via-white to-[#BBDEFB]">
+            <main>{children}</main>
+          </div>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
