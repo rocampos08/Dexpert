48dd5fffd3b8d94d7834f6849744363eb7a831d4
Rodrigo Campos <roantocampos@gmail.com>
2025-07-13 19:30:04 -0600

    feat: update font imports and change body font to Plus Jakarta Sans for improved typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,23 @@ export const metadata: Metadata = {
   }
 };
 
-import {Space_Grotesk} from "next/font/google";
+import {Space_Grotesk, Manrope, Plus_Jakarta_Sans} from "next/font/google";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-manrope",
+});
+
+
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-space-grotesk",
 });
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  variable: '--font-jakarta', // útil si usas Tailwind
+  display: 'swap',
+})
 
 export default function RootLayout({
   children,
@@ -28,7 +40,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${spaceGrotesk.className} antialiased`}>
+      <body className={`${jakarta.className} antialiased`}>
         <ClerkProvider>
           <div className="bg-gradient-to-br from-[#E3F2FD] via-white to-[#BBDEFB] min-h-screen">
             <main>{children}</main>
